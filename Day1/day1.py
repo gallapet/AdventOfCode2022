27f0c0b97cf96789938e641f59532e25fd290008
Peter Gallacher <peter.gallacher@hotmail.co.uk>
2022-12-01 15:27:43 +0000

    day 1 part 2 and refactor

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,25 +1,16 @@
 def main():
-    input_data = open('inputDay1.txt').readlines()
-    formatted_data = format_data(input_data)
-    most_calories = 0
+    input_data = open('inputDay1.txt', 'r')
+    calories_by_elf = []
     calories_sum = 0
-    for calories in formatted_data:
-        if calories != "\n":
-            calories_sum += int(calories)
-        else:
-            if calories_sum > most_calories:
-                most_calories = calories_sum
+    for calories in input_data:
+        if calories == "\n":
+            calories_by_elf.append(calories_sum)
             calories_sum = 0
-    print(most_calories)
-
-def format_data(data):
-    data_with_separators = []
-    for line in data:
-        if line != "\n":
-            data_with_separators.append(line.replace("\n", ""))
         else:
-            data_with_separators.append(line)
-    return data_with_separators
+            calories_sum += int(calories)
+    calories_by_elf.sort(reverse=True)
+    print(calories_by_elf[0])
+    print(sum(calories_by_elf[:3]))
 
 if __name__ == "__main__":
     main()
