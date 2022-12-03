0e0dfa0a061a7b14ca688b5d374b8c9406e47379
Peter <peter.gallacher@hotmail.co.uk>
2022-12-03 12:00:05 +0000

    day 3 part 2 and refactor

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,32 +1,46 @@
+priority = {x: y for (x, y) in zip('abcdefghijklmnopqrstuvwxyz', range(1, 27))}
+
 def main():
-    input_data = open('test.txt', 'r')
-    priority = {
-        'a':  1, 'b':  2, 'c':  3, 'd':  4, 'e':  5,
-        'f':  6, 'g':  7, 'h':  8, 'i':  9, 'j': 10,
-        'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15,
-        'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20,
-        'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
-        'z': 26
-    }
-    current_priority = 0
+    input_data = open('inputDay3.txt', 'r')
     total_priority = 0
+    input_lines = []
 
     for line in input_data:
         halfway_point = int((len(line) - 1) / 2)
         first_half, second_half = line[:halfway_point], line[halfway_point:]
         common_letter = find_common_letter(first_half, second_half)
-        if common_letter.isupper():
-            current_priority += 26
-        current_priority += priority[common_letter.lower()]
-        total_priority += current_priority
-        current_priority = 0
+        total_priority += get_total_priority(common_letter)
+        input_lines.append(line)
+
     print(total_priority)
+
+    groups = [input_lines[i:i + 3] for i in range(0, len(input_lines), 3)]
+    badge_total = 0
+    for group in groups:
+        badge = find_common_in_group(group)
+        badge_total += get_total_priority(badge)
+    print(badge_total)
 
 
 def find_common_letter(first_half, second_half):
     for letter in [*first_half]:
         if letter in [*second_half]:
             return letter
+
+
+def find_common_in_group(array):
+    for letter in [*array[0]]:
+        if letter in [*array[1]] and letter in [*array[2]]:
+            return letter
+
+
+def get_total_priority(letter):
+    total = 0
+    if letter.isupper():
+        total += 26
+    total += priority[letter.lower()]
+    return total
+
 
 if __name__ == "__main__":
     main()
