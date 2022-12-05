44d76025dd8fe0f60f6b23571dce90ed8191d6d5
Peter <peter.gallacher@hotmail.co.uk>
2022-12-05 19:49:51 +0000

    finish day 5

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 # read in text file
 def read_data(testing):
     if testing:
@@ -5,7 +7,7 @@ def read_data(testing):
     return open('inputDay5.txt')
 
 def main():
-    input_data = [line.replace('\n', '') for line in read_data(testing=True)]
+    input_data = [line.replace('\n', '') for line in read_data(testing=False)]
     part_one_solution = solve_part_one(input_data)
     part_two_solution = solve_part_two(input_data)
     print(f"The answer to Part 1 is: {part_one_solution}")
@@ -14,15 +16,46 @@ def main():
 def solve_part_one(data):
     blank_line_separator = data.index('')
     layout, instructions = data[:blank_line_separator], data[blank_line_separator + 1:]
-    layout_array = []
-    print(layout_array)
+    layout_map = defaultdict(list)
+    for line in layout[-2::-1]:
+        for i, char in enumerate(line[1::4], 1):
+            if char != ' ':
+                layout_map[i].append(char)
+    layout_map = do_instructions(layout_map, instructions)
+    result = []
+    for x in range(1, len(layout_map) + 1):
+        result.append(layout_map[x][-1])
+    return ''.join(result)
 
-    print(layout, instructions)
-    return
+def do_instructions(stacks, instructions):
+    for command in instructions:
+        _, count, _, old_stack, _, new_stack = command.split()
+        for i in range(int(count)):
+            stacks[int(new_stack)].append(stacks[int(old_stack)].pop())
+    return stacks
 
 def solve_part_two(data):
-    # solution goes here
-    return
+    blank_line_separator = data.index('')
+    layout, instructions = data[:blank_line_separator], data[blank_line_separator + 1:]
+    layout_map = defaultdict(list)
+    for line in layout[-2::-1]:
+        for i, char in enumerate(line[1::4], 1):
+            if char != ' ':
+                layout_map[i].append(char)
+    layout_map = do_instructions_new_crane(layout_map, instructions)
+    result = []
+    for x in range(1, len(layout_map) + 1):
+        result.append(layout_map[x][-1])
+    return ''.join(result)
+
+def do_instructions_new_crane(stacks, instructions):
+    for command in instructions:
+        _, count, _, old_stack, _, new_stack = command.split()
+        move_pile = stacks[int(old_stack)][-int(count):]
+        for i in range(0, int(count)):
+            stacks[int(new_stack)].append(move_pile[i])
+        del stacks[int(old_stack)][-int(count):]
+    return stacks
 
 if __name__ == "__main__":
     main()
