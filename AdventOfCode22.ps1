8dcb850ca99ae27413c8760236cd4aa6925370b4
Peter <peter.gallacher@hotmail.co.uk>
2022-12-06 22:10:39 +0000

    clean up script file

--- AdventOfCode22.ps1
+++ AdventOfCode22.ps1
@@ -1,13 +1,13 @@
 $day = Read-host "What day are you on?"
 
 New-Item -Path "Day$day" -ItemType Directory
-New-Item -Path ".\Day$day\sample.txt" -ItemType File
-New-Item -Path ".\Day$day\inputDay$day.txt" -ItemType File
+New-Item -Path "Day$day\sample.txt" -ItemType File
+New-Item -Path "Day$day\inputDay$day.txt" -ItemType File
 
 Copy-Item "template.py" -Destination ".\Day$day"
-Rename-Item ".\Day$day\template.py" "day$day.py"
+Rename-Item "Day$day\template.py" "day$day.py"
 
-(Get-Content -Path ".\Day$day\day$day.py" -Raw) -replace 'inputDayX.txt', "inputDay$day.txt" | Set-Content -Path ".\Day$day\day$day.py"
+(Get-Content -Path "Day$day\day$day.py" -Raw) -replace 'inputDayX.txt', "inputDay$day.txt" | Set-Content -Path "Day$day\day$day.py"
 
 cd "Day$day"
 git add . 
