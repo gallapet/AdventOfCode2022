0daecca54540b81c558705ade50b30a45c78915f
Peter Gallacher <93193880+gallapet@users.noreply.github.com>
2022-12-07 20:58:09 +0000

    day 7 part 2

--- Day7/day7.py
+++ Day7/day7.py
@@ -1,12 +1,8 @@
 # read in text file
-from collections import defaultdict
-
-
 def read_data(testing):
     if testing:
         return open('sample.txt')
     return open('inputDay7.txt')
-
 
 def main():
     input_data = [line.replace('\n', '') for line in read_data(testing=False)]
@@ -15,11 +11,10 @@ def main():
     print(f"The answer to Part 1 is: {part_one_solution}")
     print(f"The answer to Part 2 is: {part_two_solution}")
 
-
 def solve_part_one(data):
     path = []
     result = {}
-    for index, command in enumerate(data):
+    for command in data:
         split = command.split()
         if split[0] == "$":
             if split[1] == "cd":
@@ -55,8 +50,44 @@ def solve_part_one(data):
     return result
 
 def solve_part_two(data):
-    # solution goes here
-    return
+    max_space = 70000000
+    path = []
+    result = {}
+    for command in data:
+        split = command.split()
+        if split[0] == "$":
+            if split[1] == "cd":
+                if split[2] == "..":
+                    path.pop()
+                else:
+                    path.append(split[2])
+                    result.setdefault(''.join(path), [])
+        else:
+            if split[0] != "dir":
+                result[''.join(path)].append(int(split[0]))
+            else:
+                result[''.join(path)].append(split[1])
+
+    sorted_result_list = sorted(list(result.items()), key=lambda key: len(key[0]), reverse=True)
+    sorted_result = {element[0]: element[1] for element in sorted_result_list}
+
+    for key in sorted_result:
+        total = 0
+        for idx, value in enumerate(sorted_result[key]):
+            if str(value).isalpha():
+                sorted_result[key][idx] = sorted_result[key+value]
+                total += sorted_result[key][idx]
+            else:
+                total += sorted_result[key][idx]
+        sorted_result[key] = total
+
+    final = []
+    free_space = max_space - sorted_result['/']
+    for directory in sorted_result:
+        if free_space + sorted_result[directory] >= 30000000:
+            final.append(sorted_result[directory])
+
+    return min(final)
 
 if __name__ == "__main__":
     main()
